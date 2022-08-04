a02d07af3dadd901f8538caecfdb085a556d223e
AwardOfSky <34893148+AwardOfSky@users.noreply.github.com>
2022-08-05 00:11:15 +0100

    variable codomain and final transform

--- example_evoart.py
+++ example_evoart.py
@@ -1,4 +1,4 @@
-from tensorgp.tensorgp_v2 import *
+from tensorgp.engine import *
 
 # NIMA classifier imports
 from keras.models import Model
@@ -9,14 +9,22 @@ from keras.applications.mobilenet import preprocess_input as preprocess_input_mo
 
 from utils.score_utils import mean_score, std_score
 
+
 # Fitness assessment with the NIMA classifier
 # https://github.com/idealo/image-quality-assessment
 def nima_classifier(**kwargs):
     # read parameters
     population = kwargs.get('population')
+    generation = kwargs.get('generation')
     tensors = kwargs.get('tensors')
+    f_path = kwargs.get('f_path')
     objective = kwargs.get('objective')
+    _resolution = kwargs.get('resolution')
+    _stf = kwargs.get('stf')
 
+    images = True
+
+    fn = f_path + "gen" + str(generation).zfill(5)
     fitness = []
     best_ind = 0
 
@@ -31,7 +39,7 @@ def nima_classifier(**kwargs):
 
 
     number_tensors = len(tensors)
-    with tf.device('/GPU:0'):
+    with tf.device('/CPU:0'):
 
         # NIMA classifier
         x = np.stack([tensors[index].numpy() for index in range(number_tensors)], axis = 0)
@@ -40,6 +48,7 @@ def nima_classifier(**kwargs):
 
         # scores
         for index in range(number_tensors):
+
             mean = mean_score(scores[index])
             std = std_score(scores[index])
             # fit = mean - std
@@ -51,8 +60,8 @@ def nima_classifier(**kwargs):
             fitness.append(fit)
             population[index]['fitness'] = fit
 
-    # save best indiv
     return population, best_ind
+    # return population, population[best_ind]
 
 
 # if no function set is provided, the engine will use all internally available operators:
@@ -67,25 +76,26 @@ if __name__ == "__main__":
     resolution = [224, 224, 3]
 
     # GP params
-    dev = '/gpu:0'  # device to run, write '/cpu_0' to run on cpu
-    number_generations = 50
+    dev = '/gpu:0'  # device to run, write '/cpu_0' to tun on cpu
+    number_generations = 10
     pop_size = 50
     tour_size = 3
     mut_prob = 0.1
     cross_prob = 0.9
-    max_tree_dep = 15
+    max_tree_dep = 10
     # tell the engine that the RGB does not explicitly make part of the terminal set
     edims = 2
 
     # Initialize NIMA classifier
     base_model = MobileNet((None, None, 3), alpha=1, include_top=False, pooling='avg', weights=None)
-    x = Dropout(0)(base_model.output)
+    x = Dropout(0.75)(base_model.output)
     x = Dense(10, activation='softmax')(x)
     model = Model(base_model.input, x)
     model.load_weights('weights/weights_mobilenet_aesthetic_0.07.hdf5')
 
     #seed = random.randint(0, 0x7fffffff)
-    seed = 2020  # reproducibility
+    # seed = 39485793482  # reproducibility
+    seed = 2020
 
     # create engine
     engine = Engine(fitness_func=nima_classifier,
@@ -95,13 +105,13 @@ if __name__ == "__main__":
                     crossover_rate=cross_prob,
                     max_tree_depth = max_tree_dep,
                     target_dims=resolution,
-                    elitism=1,
                     method='ramped half-and-half',
                     objective='maximizing',
-                    domain_mode='clip',
-                    bloat_control='off',
                     device=dev,
                     stop_criteria='generation',
+                    codomain = [0, 1],
+                    do_final_transform = True,
+                    final_transform = [0, 255],
                     stop_value=number_generations,
                     effective_dims = edims,
                     seed = seed,
