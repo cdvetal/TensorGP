fbe1970c546fad357421eb50739bc5f6d11500bd
AwardOfSky <34893148+AwardOfSky@users.noreply.github.com>
2022-10-22 13:49:01 +0000

    Add list of expressions example

--- example_images.py
+++ example_images.py
@@ -1,13 +1,18 @@
-from tensorgp.engine import *
+from tensorgp.engine_git import *
 
 if __name__ == "__main__":
 
     # This example uses TensorGP not as an evolutionary tool but to evaluate expressions
     # converting them into images
 
-    resolution = [256, 256]
-    Engine(target_dims = resolution).generate_pop_images('images_file.txt')
+    resolution = [512, 512, 3]
 
+    # Generate from pop file
+    Engine(target_dims = resolution, effective_dims=2,).generate_pop_images('test_default.txt', 'images_dir/')
+
+    # Generate from List of strs
+    # expr = "tan(add(max(log(xor(log(add(pow(mult(exp(exp(y)), pow(sub(x, mult(y, scalar(-0.6347885638222202, 0.6011813921240905, 0.5137609112895838))), tan(x))), scalar(0.7453392097176601)), neg(x))), _and(cos(scalar(-0.6343768495230879)), y))), xor(abs(warp(y, x, scalar(-0.8560255538223858, 0.8518172740714811, -0.09226456331173094))), sin(scalar(-0.7513214358991382, 0.5814230533953741, 0.7537424409867375)))), y))"
+    # Engine(target_dims = resolution, effective_dims=2).generate_pop_images([expr], 'images_dir/')
 
     """
     Notes:
