d6552f2e6b6b8d9ccac30c1289905dd07db02344
AwardOfSky <34893148+AwardOfSky@users.noreply.github.com>
2024-12-14 16:02:58 +0000

    license fix

--- tensorgp/engine.py
+++ tensorgp/engine.py
@@ -1,9 +1,9 @@
 #MIT License
 
-#Copyright (c) 2020-2021 Francisco Baeta (University of Coimbra)
-#Copyright (c) 2020-2021 João Correia (University of Coimbra)
-#Copyright (c) 2020-2021 Tiago Martins (University of Coimbra)
-#Copyright (c) 2020-2021 Penousal Machado (University of Coimbra)
+#Copyright (c) 2020-present Francisco Baeta (University of Coimbra)
+#Copyright (c) 2020-present João Correia (University of Coimbra)
+#Copyright (c) 2020-present Tiago Martins (University of Coimbra)
+#Copyright (c) 2020-present Penousal Machado (University of Coimbra)
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
